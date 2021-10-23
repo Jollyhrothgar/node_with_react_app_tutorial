b53ce3f5c7b8045a1d149cf8356ee2f4c4f53ced
Mike Beaumier <michael.beaumier@gmail.com>
2021-10-22 18:14:53 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Full Stack React
+
+I'm taking this
+[course](https://www.udemy.com/course/node-with-react-fullstack-web-development/learn/lecture/7593644#overview)
+to figure out how to build banjo apps.
+
+## Links
+
+- Course repository: https://github.com/StephenGrider/FullstackReactCode
+- Auto-formatting JS: https://github.com/prettier/prettier
+- Udemy discussion thread (in video)
+
+## Notes
+
+### 22 October 2021
+
+**`npm init`**
+
+Creates a `package.json` file which is used to describe the project and manage
+dependencies.
+
+**Node**
+
+Executes javascript outside of the browser.
+
+**Express**
+
+A library that runs in the Node runtime. Has helpers to manage HTTP traffic.
+Express manages route handlers.
+
+**Running the server**
+
+The server is run with `node index.js`.
+
+**Deployment**
+
+Heroku, AWS, Digital Ocean, etc. Heroku is free to get started with.
+
+- Dynamic port binding
+- Node environment
+- Start script
+- .gitignore
+
+*First time Deployment*
+
+Heroku uses a git-based deployment workflow.
+
+*Subsequent Deployments*
---
+++ server/index.js
@@ -0,0 +1,43 @@
+// Common js modules on server-side. Other import syntax looks like
+//    import express from 'express'
+
+// This is the server side 'import statement'
+const express = require('express');
+
+// This represents a running instance of express - the app.
+// It listens for traffic and routes to handlers.
+const app = express();
+
+// Create a route-handler (arg1, arg2) => {} is an anonymous function
+// definition, its like a lambda.  Sorta.
+//
+// app: the running express server
+// app.get: creating a new route handler
+// app.get('/' : watch for requests accessing '/' 
+//     get: get info
+//     post: send info
+//     put: update all the properties of something
+//     delete: delete something
+//     patch: update one or two properties of something.
+//
+// app.get takes two arguments: the first one is the route, the second one is
+// the handler.
+//
+// req: incoming request
+// resp: outgoing response
+// res.send: immediate response
+app.get('/', (req, res) => {
+	res.send({hi: 'there'});
+});
+
+// Figure out port dynamically
+// heroku grabs the port from environment, so we look this up dynamically.
+// 
+// If there is not an env variable assigned by heroku, use the default value
+// of 5000;
+// 
+// This is sort of like a ternary operator in python.
+const PORT = process.env.PORT || 5000;
+
+// listens on port 500.
+app.listen(PORT);
